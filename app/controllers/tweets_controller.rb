57c817fd35f9f4addb7cd7096212d656635dd488
Nathan Specht <nathanspecht@gmail.com>
2016-04-20 13:33:38 -0700

    Expire cache after 5 minutes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,25 +3,21 @@ class TweetsController < ApplicationController
 
   def index
     options = { count: TWEET_COUNT }
-    # Rails.cache.fetch("#{username}", expires_in: 5.minutes) do
-    @tweets = Rails.cache.read(username)
-    if !@tweets
-      @tweets = twitter_client.
-        user_timeline(username, options).
-        map do |tweet|
-          { 
-            id: tweet.id,
-            text: tweet.text, 
-            created_at: tweet.created_at,
-            user: {
-              image_url: tweet.user.profile_image_uri.to_s,
-              username: tweet.user.username,
-              name: tweet.user.name
-            }
+    @tweets = Rails.cache.fetch(username, expires_in: 5.minutes) do 
+      twitter_client.
+      user_timeline(username, options).
+      map do |tweet|
+        { 
+          id: tweet.id,
+          text: tweet.text, 
+          created_at: tweet.created_at,
+          user: {
+            image_url: tweet.user.profile_image_uri.to_s,
+            username: tweet.user.username,
+            name: tweet.user.name
           }
-        end
-      Rails.cache.write(username, @tweets, exipres_in: 30.seconds) 
-    # end
+        }
+      end
     end
   end
 
